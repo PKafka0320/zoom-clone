41bd49fcca6d839f72f22daba23cb6b972b7c0ae
PKafka0320 <aiya0422@gmail.com>
2022-06-27 17:32:32 +0900

    fix comments, add redirection

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug"); // setting view engine
-app.set("views", __dirname + "/views"); // setting directory of viewers
+app.set("views", __dirname + "/views"); // setting directory of views
 
-// permission for access to folder
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); // setting directory of files
+// public files will be executed in frontend
 
 // rendering for each address
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 // opening port
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
